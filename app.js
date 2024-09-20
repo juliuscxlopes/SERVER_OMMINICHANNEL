00507ebbd6b2f052d08f8f98d1192767a8484fc1
Julius Lopes <juliuscxlopes@gmail.com>
2024-09-20 11:55:34 -0300

    implantando o banco

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ app.use(express.json());
 // Rotas
 app.use('/attendants', attendantsRoutes);
 
+// Função para iniciar consumidores
+const startConsumers = async (consumers) => {
+  for (const consumer of consumers) {
+    await consumer(); // Chama a função de cada consumidor
+  }
+};
 
 // Iniciar servidor e conectar ao Kafka
 app.listen(port, async () => {
@@ -25,10 +31,6 @@ app.listen(port, async () => {
     // Conectar ao Kafka
     await connectKafka();
     console.log('Conectado ao Kafka');
-
-    // Iniciar consumidores
-    await startConsumers([startRegisterConsumers, startLoginConsumers]);
-    console.log('Todos os consumidores Kafka iniciados');
 
   } catch (error) {
     console.error('Erro ao conectar ao Kafka ou iniciar consumidores:', error.message);
