0220fe30395b3c390b2cb05bb2b3035bcc18362d
Julius Lopes <juliuscxlopes@gmail.com>
2024-09-20 11:13:48 -0300

    implantando o banco

--- src/routes/attendantsRoutes/authRoutes.js
+++ src/routes/attendantsRoutes/authRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../../controllers/attendants/authController');
+const { loginUser } = require('../../controllers/attendants/login/controllerReqLogin');
+const { registerUser } = require('../../controllers/attendants/register/controllerReqRegister');
+
 
 // Rota de cadastro
 router.post('/register', registerUser);
